524bc4a3dc448355f6928dac5a6dcb9dc767954d
Mitali Sengupta <33852986+MitaliSengupta@users.noreply.github.com>
2019-04-22 20:07:49 -0700

    replacing % with P

--- 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -6,7 +6,7 @@ class Program
 	{
 		double percent = .7553;
 		double currency = 98765.4321;
-        Console.WriteLine("Percent: {0:0.00%}", percent);
+        Console.WriteLine("Percent: {0:P}", percent);
         Console.WriteLine("Currency: {0:C}", currency);
 	}
 }
