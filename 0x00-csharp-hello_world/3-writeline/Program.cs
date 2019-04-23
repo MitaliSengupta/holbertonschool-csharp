7b9ca92f20e71850c159d122861eb0dd7b798831
Mitalisengupta <mitali.s.auger@gmail.com>
2019-04-22 18:34:16 -0700

    removed wrong file name

--- 0x00-csharp-hello_world/3-writeline/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-class Program
-{
-        static void Main(string[] args)
-        {
-            Console.WriteLine("\"Programming is like building a multilingual puzzle");
-        }
-}
-
