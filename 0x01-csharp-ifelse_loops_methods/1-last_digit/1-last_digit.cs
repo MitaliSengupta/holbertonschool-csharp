1005ffce39a09b8ebddcc630a183753940bfcf91
Mitalisengupta <mitali.s.auger@gmail.com>
2019-04-24 12:39:32 -0700

    checking for last digit values

---
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10000, 10000);
+        int digit = number % 10;
+        if (digit < 6 && digit != 0)
+        {
+            Console.WriteLine("The last digit of {0:D} is {1:D} and is less than 6 and not 0", number, digit);
+        }
+        else if (digit > 5)
+        {
+            Console.WriteLine("The last digit of {0:D} is {1:D} and is greater than 5", number, digit);
+        }
+        else
+        {
+            Console.WriteLine("The last digit of {0:D} is {1:D} and is 0", number, digit);
+        }
+    }
+}
