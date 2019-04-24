2e48f7a61b02f9f4d9370981e988759353072b73
Mitalisengupta <mitali.s.auger@gmail.com>
2019-04-24 13:11:22 -0700

    printing hex values of numbers

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int i;
+        for (i = 0; i <=98; i++)
+        {
+            Console.WriteLine("{0:D} = 0x{0:X}", i);
+        }
+    }
+}
