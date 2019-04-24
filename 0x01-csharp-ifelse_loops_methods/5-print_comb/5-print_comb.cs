ea193f0f88b7ebcad5a589388a290ecb8ea3929d
Mitalisengupta <mitali.s.auger@gmail.com>
2019-04-24 15:23:35 -0700

    printing upto 2 digits

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string comma = "";
+        for(int i = 0; i <= 99; i++)
+        {
+            Console.Write("{0}{1:D2}", comma, i);
+            comma = ", ";
+        }
+        Console.WriteLine();
+    }
+}
