a362426ef91bd8ec7cd253f5791cc01a1d06c21f
Mitalisengupta <mitali.s.auger@gmail.com>
2019-04-24 13:06:12 -0700

    printing alphabets with omitted letters

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        char i;
+        for (i = 'a'; i <= 'z'; i++)
+        {
+            if (i == 'e' || i == 'q')
+            {
+                i++;
+            }
+            Console.Write(i);
+        }
+    }
+}
