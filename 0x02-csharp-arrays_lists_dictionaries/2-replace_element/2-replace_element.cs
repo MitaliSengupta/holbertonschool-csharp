f71397117ab8b9605121790e5cdfda633fe8c1b1
Mitalisengupta <mitali.s.auger@gmail.com>
2019-05-28 02:50:16 -0700

    replacing element at given index

---
+++ 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Array
+{
+    public static int[] ReplaceElement(int[] array, int index, int n)
+    {
+        if (index < 0 || index >= array.Length)
+        {
+            Console.WriteLine("Index out of range");
+            return(array);
+        }
+        array[index] = n;
+        return(array);
+    }
+}
