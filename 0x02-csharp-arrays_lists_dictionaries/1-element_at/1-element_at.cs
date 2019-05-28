f9b891a3ce6905072b5b09032d90d1dac8b3be39
Mitalisengupta <mitali.s.auger@gmail.com>
2019-05-28 02:49:40 -0700

    finding element at given index

---
+++ 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Array
+{
+    public static int elementAt(int[] array, int index)
+    {
+        if (index < 0 || index >= array.Length)
+        {
+            Console.WriteLine("Index out of range");
+            return(-1);
+        }
+        return (array[index]);
+    }
+}
