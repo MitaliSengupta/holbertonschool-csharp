5df0dfe6245ea0fbf2139ae6a6eafaf6e08b11f4
Mitalisengupta <mitali.s.auger@gmail.com>
2019-05-28 02:48:38 -0700

    printing array

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Array
+    {
+        public static int[] CreatePrint(int size)
+        {
+            if (size < 0)
+            {
+                Console.WriteLine("Size cannot be negative");
+                return null;
+            }
+            int[] num = new int[size];
+            for(int i = 0; i < size; i++)
+            {
+                num[i] = i;
+            }
+            Console.WriteLine(String.Join(" ", num));
+            return num;
+        }
+
+    }
+
