0a83998e66d20b5f20e311677863e3a0009abb73
Mitalisengupta <mitali.s.auger@gmail.com>
2019-06-12 04:28:27 -0700

    updated file

--- 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -8,6 +8,7 @@ class List
         if (index < 0 || index > myList.Count)
         {
             Console.WriteLine("Index is out of range");
+            return;
         }
         else
         {
