c3d5b096da32c6b29253af87aa9cd0448ba7642f
Mitalisengupta <mitali.s.auger@gmail.com>
2019-06-12 08:54:34 -0700

    encompassing limits for if check

--- 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -5,7 +5,7 @@ class LList
 {
     public static int Pop(LinkedList<int> myLList)
     {
-        if (myLList.Count < 0)
+        if (myLList.Count <= 0)
         {
             return 0;
         }
