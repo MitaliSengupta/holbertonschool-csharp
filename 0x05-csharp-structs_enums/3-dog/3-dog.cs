1f99b71f6dc8d9aba8b12ffc7691ec5d558270f9
Mitalisengupta <mitali.s.auger@gmail.com>
2019-06-18 01:30:04 -0700

    overriding val types with ToString method

--- 0x05-csharp-structs_enums/3-dog/3-dog.cs
+++ 0x05-csharp-structs_enums/3-dog/3-dog.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 
-namespace _3_dog
+public enum Rating {Good, Great, Excellent}
+public struct Dog
 {
-    class Program
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+    
+    public Dog(string nam, float ag, string own, Rating rat)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        name = nam;
+        age = ag;
+        owner = own;
+        rating = rat;
+    }
+
+    public override string ToString()
+    {
+        return String.Format("Dog Name: {0}\nAge: {1}\nOwner: {2}\nRating: {3}", name, age, owner, rating);
+
     }
 }
