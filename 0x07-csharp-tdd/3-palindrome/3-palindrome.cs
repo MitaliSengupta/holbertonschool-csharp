9669763532d5d3ca2572eb81e09960c9e72a242a
Mitalisengupta <mitali.s.auger@gmail.com>
2019-06-18 02:43:53 -0700

    initial commit

---
+++ 0x07-csharp-tdd/3-palindrome/3-palindrome.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _3_palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 0x07-csharp-tdd/5-camelcase/5-camelcase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _5_camelcase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
