3e227946b021cf4819ad2a72ed55989ec7e677e5
Mitalisengupta <mitali.s.auger@gmail.com>
2019-06-18 01:56:18 -0700

    adding method to get health for diff objects

--- 0x06-csharp-classes/3-enemy/3-enemy.cs
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 
-namespace _3_enemy
+namespace Enemies
 {
-    class Program
+    /// <summary>
+    /// adding fields and constructor to the class setting the default values to zero
+    /// </summary>
+    public class Zombie
     {
-        static void Main(string[] args)
+        ///<summary> defining a private field for zombie called health </summary>///
+        private int health;
+        /// <summary> setting the health to 0 via the constructor </summary>///
+        public Zombie()
         {
-            Console.WriteLine("Hello World!");
+            health = 0;
+        }
+        ///<summary> overriding constructor to get and set value </summary>///
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            else
+            {
+                health = value;
+            }
+        }
+        ///<summary> method that returns the value of health of the zombie object </summary>///
+        public int GetHealth()
+        {
+            return health;
         }
     }
 }
