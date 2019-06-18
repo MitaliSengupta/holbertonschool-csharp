ea245316bb243a1d9599affc03976192f23d5f5d
Mitalisengupta <mitali.s.auger@gmail.com>
2019-06-18 00:34:08 -0700

    initial commit

---
+++ 0x05-csharp-structs_enums/0-dog/0-dog.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _0_dog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
